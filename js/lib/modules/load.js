17e97421d1885853b4e8499fe91182d9ba29c088
Chris Griffith <chrisg@terraeclipse.com>
2014-04-03 13:40:19 -0400

    Adds randomply placed images

--- js/lib/modules/load.js
+++ js/lib/modules/load.js
@@ -8,7 +8,8 @@ define(['jquery'],function( $ ) {
       $img = $('.avatar-feed li img'),
       $count = $('#signerCount'),
       last = $avatar.length,
-      count = 0;
+      count = 0,
+      window_h;
 
   // Animate count up
   var counter = setInterval(function() {
@@ -16,15 +17,20 @@ define(['jquery'],function( $ ) {
     $count.html(count);
   },25);
 
+  window_h = $(window).height();
+
   // Load each avatar sequentially as the count animates up
   $avatar.each(function(i) {
 
     // Move avatar to a random position
     var random_pos = Math.floor((Math.random()*200)+1);
     $avatar.eq(random_pos).after($(this));
-    
+
+    var random_top = Math.floor((Math.random()*(window_h-150))+1);
+    $(this).css('top',random_top);
+
     // Use delay to offset the fade on each avatar
-    $('img',this).delay(i*25).fadeIn(100,function() {
+    $('img',this).delay(i*25).fadeIn(250,function() {
       // When the last avatar is reached
       if (i===last-1) {
         // Animate in the map and layers
@@ -40,6 +46,7 @@ define(['jquery'],function( $ ) {
   // Keep the avatars square on window load/resize
   $(window).on('load resize',function() {
     setAvatarSquare();
+    window_h = $(window).height();
   });
 
   function setAvatarSquare() {
