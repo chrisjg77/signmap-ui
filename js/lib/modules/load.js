4ed5d084e4e90369f44e6e48b2d4bc57791aad6a
Chris Griffith <chrisg@terraeclipse.com>
2014-04-03 10:43:33 -0400

    Add avatar feed and loading count number animations

--- js/lib/modules/load.js
+++ js/lib/modules/load.js
@@ -2,8 +2,44 @@
 // Define this module and make it dependent on jquery.
 define(['jquery'],function( $ ) {
   
-  setTimeout(function() {
-    $('body').addClass('loaded');
-  },1000);
+  // setTimeout(function() {
+  //   $('body').addClass('loaded');
+  // },1000);
+
+  var $this = $(this),
+      $avatar = $('.avatar-feed li'),
+      $count = $('#signerCount'),
+      last = $avatar.length,
+      count = 0;
+
+  var counter = setInterval(function() {
+
+    count = count+25;
+    $count.html(count);
+
+  },25);
+
+  $avatar.each(function(i) {
+    $(this).delay(i*25).fadeIn(100,function() {
+      if (i===last-1) {
+        setTimeout(function() {
+          $('body').addClass('loaded');
+        },750);
+        var header_h = $('header').height();
+        $('main').css('padding-top',header_h);
+        clearInterval(counter);
+      }
+    });
+    
+  });
+
+  $(window).on('load resize',function() {
+    setAvatarSquare();
+  });
+
+  function setAvatarSquare() {
+    var avatar_w = $avatar.width();
+    $avatar.css('height',avatar_w);
+  }
 
 });
